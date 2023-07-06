48e982255d41457354ddc0bae9ac222031bfdbe3
alaskannate <64567768+alaskannate@users.noreply.github.com>
2023-07-06 11:49:31 -0700

    typo fix

--- auto-commit-to-git.py
+++ auto-commit-to-git.py
@@ -1,29 +1,33 @@
 import os
 import subprocess
 
-def get_git_status(folder_path):
+def git_add(root_folder_path):
     # Change the current working directory to the folder path
-    os.chdir(folder_path)
+    os.chdir(root_folder_path)
+    
+    # Run git add command 
+    add = subprocess.run(['git', 'add', '.' ], capture_output=True, text=True)
 
-    # Run git status command
-    result = subprocess.run(['git', 'status'], capture_output=True, text=True)
-
+    # Run git commit command with "auto comment"
+    commit = subprocess.run(['git', 'commit', '-m','typo fix'], capture_output=True, text=True)
     # Return the captured output
-    return result.stdout.strip()
 
-def parse_folder_git_status(root_folder):
+    return print(add.stdout.strip())
+
+def parse_folder_and_add(root_folder_path):
     # Iterate through each subfolder
-    for folder_name in os.listdir(root_folder):
-        folder_path = os.path.join(root_folder, folder_name)
+    for folder_name in os.listdir(root_folder_path):
+        folder_path = os.path.join(root_folder_path , folder_name)
 
         # Check if the path is a directory
-        if os.path.isdir(folder_path):
-            print(f"Git status for folder: {folder_name}")
-            print(get_git_status(folder_path))
-            print()
+        if os.path.isdir(folder_path) and not folder_name.startswith('.'):
+            print(f"Git added folders name: {folder_name}")
+            git_add(folder_path)
+
 
 # Provide the root folder path
-root_folder_path = '/path/to/root/folder'
+root_folder_path = '/Users/nathanielewing/Desktop/Code/Current-Projects/'
+
 
 # Call the function to parse and display Git status
-parse_folder_git_status(root_folder_path)
+parse_folder_and_add(root_folder_path)
