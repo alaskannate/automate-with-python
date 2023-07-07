3db2ebf2d3ec789aee46be7cfafa4cd87a1e3f86
alaskannate <64567768+alaskannate@users.noreply.github.com>
2023-07-06 17:03:50 -0700

    typo

--- auto-commit-to-git.py
+++ auto-commit-to-git.py
@@ -1,49 +1,60 @@
 import os
 import subprocess
 
-def git_commit_to_github(folder_path):
-    # Change the current working directory to the folder path
+
+
+def git_commit_to_github(folder_path, folder_name, files_to_exclude):
+
+
+    # Change the current working directory to the folder path.
     os.chdir(folder_path)
 
-    # Run git status command
+
+    # convert excluded files to string.
+    excluded_files_str = ", ".join(files_to_exclude)
+
+    # git command varables.
     status = subprocess.run(['git', 'status'], capture_output=True, text=True)
+
+    add = subprocess.run(['git', 'add', '.', '--exclude'] + files_to_exclude, capture_output=True, text=True)
     
-    add = subprocess.run(['git', 'add', '.'], capture_output=True, text=True)
-
-    # Check if there are any changes to commit
-    if 'nothing to commit' not in status.stdout:
-        # Run git add command 
-        add
-        print("has been pushed to github.")
-    else:
-        print("Has no changes to commit.")
-
-    print('\n')
-
-    # Run git commit command with "auto comment"
     commit = subprocess.run(['git', 'commit', '-m','typo fix'], capture_output=True, text=True)
 
-    # upload to gitgub.
     upload_to_github = subprocess.run(['git','push','origin','main'], capture_output=True, text=True)
 
-    # Return the captured output
+    # Check if there are any changes to commit.
+    if 'nothing to commit' not in status.stdout:
 
-    return add.stdout.strip()
+            
+        add
+            
+        commit
+
+        upload_to_github
+        print(folder_name.capitalize() + " has been pushed to github." + " excluding files inisde of " + excluded_files_str )
+
+    else:
+        print(folder_name.capitalize() + " has no changes to commit.")
+
 
 
 def parse_folder_for_subfolder_names(root_folder_path):
+
+
     # Iterate through each subfolder
     for folder_name in os.listdir(root_folder_path):
         folder_path = os.path.join(root_folder_path , folder_name)
 
         # Check if the path is a directory
-        if os.path.isdir(folder_path) and not folder_name.startswith('.'):
-            print ("The project: " + folder_path)
-            git_commit_to_github(folder_path)
+
+        if os.path.isdir(folder_path):
+            git_commit_to_github(folder_path, folder_name , files_to_exclude)
         
 
 # Provide the root folder path
-root_folder_path = '/Users/nathanielewing/Desktop/Code/Current-Projects/'
+root_folder_path = '/Users/nathanielewing/Desktop/Code/My-Current-Projects/'
+# List files to ignore 
+files_to_exclude = ['.gitignore']
 
 # Call the function to parse and display Git status
 parse_folder_for_subfolder_names(root_folder_path)
